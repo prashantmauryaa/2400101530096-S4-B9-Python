39ebc3622c18ef329dd8ab2ffcdaac965ee6260a
MasterMaurya <mauryaprashant270@gmail.com>
2026-03-17 13:11:17 +0530

    Add React JSX practice: variables, functions, and calc() with operations

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,15 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Demo} from './Component/Demo'
+
 
 function App() {
-  
+
+  let x = 20;
+  let y = 19;
+  function name() {
+    return "My name is Prashant Maurya";
+  }
+  function sum(a, b) {
+    return a + b;
+  }
+  function calc(a, b, operation) {
+    if (operation === "+") return a + b;
+    if (operation === "-") return a - b;
+    if (operation === "*") return a * b;
+    return "Invalid operation";
+  }
+
+
+
+
 
   return (
     <>
-      <h1>This is Prashant Maurya Doing Import and Export from the Demo Component</h1>
-      <Demo />
+      <h1> User variable with jsx</h1>
+
+      <h3>Value of variable is: {x + y}</h3>
+      <h2>{name()}</h2>
+      <h2>{sum(x, y)}</h2>
+      <h2>calc({x}, {y}, "+") = {calc(x, y, "+")}</h2>
+      <h2>calc({x}, {y}, "-") = {calc(x, y, "-")}</h2>
+      <h2>calc({x}, {y}, "*") = {calc(x, y, "*")}</h2>
     </>
   )
 }
