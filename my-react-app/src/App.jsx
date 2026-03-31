c0e602a639956d2fda5be5a714cee7d56e85357b
MasterMaurya <mauryaprashant270@gmail.com>
2026-03-31 13:05:39 +0530

    Added OK button and Fruit logic to React App

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,36 +5,31 @@ import './App.css'
 
 
 function App() {
-
-  let x = 20;
-  let y = 19;
-  function name() {
-    return "My name is Prashant Maurya";
+  // Function to handle fruit button clicks
+  function FruitClick(fruitName) {
+    alert("you click on " + fruitName);
   }
-  function sum(a, b) {
-    return a + b;
-  }
-  function calc(a, b, operation) {
-    if (operation === "+") return a + b;
-    if (operation === "-") return a - b;
-    if (operation === "*") return a * b;
-    return "Invalid operation";
-  }
-
-
-
-
 
   return (
     <>
-      <h1> User variable with jsx</h1>
+      <div>
+        <button onClick={() => FruitClick("apple")}>
+          Apple
+        </button>
+        <button onClick={() => FruitClick("banana")}>
+          Banana
+        </button>
+      </div>
 
-      <h3>Value of variable is: {x + y}</h3>
-      <h2>{name()}</h2>
-      <h2>{sum(x, y)}</h2>
-      <h2>calc({x}, {y}, "+") = {calc(x, y, "+")}</h2>
-      <h2>calc({x}, {y}, "-") = {calc(x, y, "-")}</h2>
-      <h2>calc({x}, {y}, "*") = {calc(x, y, "*")}</h2>
+      <div>
+        <button onClick={() => alert("click on ok")}>
+          OK
+        </button>
+      </div>
+
+      <div>
+        <h1>Welcome to New Project </h1>
+      </div>
     </>
   )
 }
